c41146dc8954e1b47ad1a7003603665d0138acb2
RenatoDevv <apazaarenatogmail.com>
2023-05-02 13:37:37 -0500

    Actualizacion de index and css

--- index.php
+++ index.php
@@ -5,42 +5,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="./css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Index</title>
 </head>
 
 <body>
-    <header>
-        <h1 class="logo">LIVEFE</h1>
+    <header class="header">
+        <a href="index.php"><h1 class="logo">LIVEFE</h1></a>
         <nav>
             <a href="" class="navegar">Home</a>
             <a href="" class="navegar">Services</a>
-            <a href="" class="navegar">Contacto</a>
-            <a href="" class="navegar">Servicios</a>
+            <a href="" class="navegar">Contact</a>
+            <a href="" class="navegar">About</a>
             <a href="auth/register.php" class="navegar inscrib">Inscribirse</a>
         </nav>
     </header>
     <main>
-        <div class="content">
-            <div class="texto">
-                <h2>EL MEJOR SITIO PARA PUBLICIDAD</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem nemo odio laboriosam consequatur neque
-                    voluptates, inventore pariatur libero similique atque explicabo nisi, distinctio dicta, vero tempora
-                    et voluptatibus ab corrupti.</p>
-                <a href="" class="link-in">Inscribirse</a>
-            </div>
-            <div class="image-con">
-                <img src="" alt="">
-            </div>
-        </div>
         <section>
-            <h2>THE OLD FORESH HAMBER</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur itaque quaerat esse nulla impedit.</p>
-
+            <h2>ENCIENDE EL CARRO</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quis eum blanditiis totam quos inventore cupiditate molestias magni itaque consectetur est asperiores ducimus velit accusamus quaerat, veniam dolorem, accusantium assumenda?</p>
+            <a>Get Started</a>
         </section>
     </main>
-
 </body>
 
 </html>
