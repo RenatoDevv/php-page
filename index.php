70527422f27aec0163c0bf1929b8ee92e21fedcf
RenatoDevv <apazaarenatogmail.com>
2023-05-01 22:48:30 -0500

    Actualizacion de pages

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,30 +9,38 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Index</title>
 </head>
+
 <body>
     <header>
-        <div class="image">
-            <h1>Hello</h1>
-        </div>
+        <h1 class="logo">LIVEFE</h1>
         <nav>
             <a href="" class="navegar">Home</a>
             <a href="" class="navegar">Services</a>
             <a href="" class="navegar">Contacto</a>
             <a href="" class="navegar">Servicios</a>
-            <a href="register.php" class="navegar inscrib">Inscribirse</a>
+            <a href="auth/register.php" class="navegar inscrib">Inscribirse</a>
         </nav>
     </header>
-    <div class="content">
-        <div class="texto">
-            <h2>EL MEJOR SITIO PARA PUBLICIDAD</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem nemo odio laboriosam consequatur neque 
-                voluptates, inventore pariatur libero similique atque explicabo nisi, distinctio dicta, vero tempora 
-                et voluptatibus ab corrupti.</p>
-            <a href="" class="link-in">Inscribirse</a>
+    <main>
+        <div class="content">
+            <div class="texto">
+                <h2>EL MEJOR SITIO PARA PUBLICIDAD</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem nemo odio laboriosam consequatur neque
+                    voluptates, inventore pariatur libero similique atque explicabo nisi, distinctio dicta, vero tempora
+                    et voluptatibus ab corrupti.</p>
+                <a href="" class="link-in">Inscribirse</a>
+            </div>
+            <div class="image-con">
+                <img src="" alt="">
+            </div>
         </div>
-        <div class="image-con">
-            <img src="" alt="">
-        </div>
-    </div>
+        <section>
+            <h2>THE OLD FORESH HAMBER</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur itaque quaerat esse nulla impedit.</p>
+
+        </section>
+    </main>
+
 </body>
+
 </html>
